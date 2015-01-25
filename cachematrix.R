0df88567e2b5084b6252f7d7998f936338f3ac78
Abelard Ma <abelardma@abelards-mbp.home>
2015-01-25 14:24:22 -0500

    Assignment2
    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions will cache the inverse of a matrix.
 
-## Write a short comment describing this function
+##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL  	##Setting m to NULL, so that in effect m would be created in this function and R would not search for m elsewhere in 'higher' environments.
+  set<-function(y){
+    x<<-y   ##Took input and assigned it to x in the parent environment of the function.
+    m<<-NULL
+  }
+  get<-function() x   ##returned 'x' from its parent environment. 
+  setinverse<-function(solve)m<<-solve
+  getinverse<-function()m		##Assigning the local m to getinverse.
+  list(set=set, get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if(!is.null(m)){							##The variable m from the parent environment of the 'makeCacheMatrix' function is assigned the value of the mean using the 'getinverse' method of the makeCacheMatrix function.
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()		
+  m<-solve(data,...)
+  x$setinverse(m)  ##Passing the inverse to the setinverse method of the makeCacheMatrix function. Assigning the inverse to the 'm' variable in the parent environment of the makeCacheMatrix function.
+  m
 }
